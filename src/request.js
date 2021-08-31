1b49287e44572b8fa32afb6262d9c327374cef83
jomin398 <jomin398@gmail.com>
2021-08-31 17:22:35 +0900

    update

---
+++ src/request.js
@@ -0,0 +1,31 @@
+const Lang = navigator.language;
+const isKor = Lang == 'ko-KR';
+const DB = {};
+//Localiztion
+function loadLocalFile(p,cbf){
+    let xhr = new XMLHttpRequest();
+    if(!cbf){
+        throw 'Call Back Function is Undefined';
+    }
+    xhr.open('GET',p,true);
+    xhr.onload =()=> cbf(xhr.responseText);
+    xhr.send();
+}
+function Localiztion(d){
+    DB.json = JSON.parse(d);
+    document.getElementById('title').innerText = DB.json[0].t;
+}
+
+function menuFOS(){
+    let d = document.querySelector('#search input').value;
+    console.log(d);
+}
+window.onload = ()=>{
+    console.log(isKor);
+    loadLocalFile('./asset/local_ko.json',Localiztion);
+    document.querySelector('#search button').addEventListener("click", menuFOS);
+    /*document.body.onload = ()=> {
+        //document.getElementById('title').innerText = 'akaka'
+        console.log(isKor);
+    }*/
+}
--- test/src/request.js
+++
@@ -1,9 +0,0 @@
-const Lang = navigator.language;
-const isKor = Lang == 'ko-KR';
-window.onload = ()=>{
-    console.log(isKor);
-    /*document.body.onload = ()=> {
-        //document.getElementById('title').innerText = 'akaka'
-        console.log(isKor);
-    }*/
-}
