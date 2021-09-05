6e009e7a56031214b66ade87d646d56cbebfa56f
jomin398 <jomin398@gmail.com>
2021-09-05 21:14:56 +0900

    update

--- src/request.js
+++ src/request.js
@@ -53,11 +53,45 @@ function displayInfo(str) {
         ele.id = 'displayInfo';
         ele.innerText = '[to Show String Here]';
         document.body.appendChild(ele)
-    } else {
-        if (str) {
-            ele.innerText = Array.isArray(str) ? str.join('\n') : typeof str == 'object' ? JSON.stringify(str) : str;
-        }
     }
+    if (str) {
+        ele.innerText = Array.isArray(str) ? str.join('\n') : typeof str == 'object' ? JSON.stringify(str) : str;
+    }
+}
+function blink(targetElem, showMsg) {
+    targetElem.classList.add("blink");
+    if (showMsg) {
+        displayInfo(showMsg);
+    }
+    setTimeout(() => {
+        targetElem.classList.remove('blink');
+        if (document.getElementById('displayInfo')) {
+            document.getElementById('displayInfo').remove();
+        }
+    }, 2000);
+}
+function makeUserIO() {
+    let list = null;
+    DB.user = {};
+    //make await for jsonRenderer
+    let timer = setInterval(() => {
+        if (document.querySelector('#json-rendrer')) {
+            list = document.querySelector('#json-rendrer > ol > li:nth-child(2) > ul > li:nth-child(3) > ol').childNodes;
+            // append Buttons on beside of BeatMap setIDs
+            for (el of list) {
+                let bn = document.createElement('button');
+                bn.type = 'button';
+                bn.innerText = "select This";
+                bn.onclick = () => {
+                    DB.user.SelectedBeatMapSetID = bn.parentElement.getElementsByClassName('json-literal')[0].innerText;
+                    displayInfo("Requester: Selected BeatMap's SetId : " + DB.user.SelectedBeatMapSetID)
+                    console.log(DB.user.SelectedBeatMapSetID);
+                };
+                el.querySelector('.json-dict').firstChild.append(bn);
+            }
+            clearInterval(timer);
+        }
+    }, 1000);
 }
 function menuFOS() {
     DB.search = document.querySelector('#search input').value.trim();
@@ -66,31 +100,64 @@ function menuFOS() {
         const params = new URLSearchParams();
         params.append('query', DB.search);
         params.append('amount', 20);
-        params.append('status', 1); // ranked;
+        params.append('status', 0); // ranked;
         params.append('offset', 0);
         document.querySelector('#search').remove();
         displayInfo("Requester: Requesting from bloodCat...")
         loadXhr("https://api.chimu.moe/v1/search?" + params.toString(), function (res) {
             DB.json = JSON.parse(res);
-            DB.json.data.forEach((e) => {
-                delete e.ChildrenBeatmaps;
-                e.Title.trim();
-            }) // delete ChildrenBeatmaps Obj
-            //DB.json.data = DB.json.data.filter(e => (e.Title == DB.search || e.Title.toLowerCase() == DB.search.toLowerCase()))
-            displayInfo("Requester: Look at JSON View!")
-            displayJSON([params.toString(), DB.json])
+            if (DB.json.code == 0) {
+                DB.json.data.forEach((e) => {
+                    delete e.ChildrenBeatmaps;
+                    e.Title.trim();
+                }) // delete ChildrenBeatmaps Obj
+                //DB.json.data = DB.json.data.filter(e => (e.Title == DB.search || e.Title.toLowerCase() == DB.search.toLowerCase()))
+                displayInfo("Requester: Look at JSON View!")
+                displayJSON([params.toString(), DB.json]);
+                makeUserIO();
+            } else {
+                displayInfo("Requester: " + DB.json.message + " (" + DB.json.code + ")")
+                //document.getElementById('jsonWrapper').remove();
+                initSearch();
+                blink(document.querySelector('#search input[type=text]:required'), DB.json.message);
+            }
+
         });
     } else {
-        document.querySelector('#search input[type=text]:required').classList.add("blink");
-        setTimeout(() => {
-            document.querySelector('#search input[type=text]:required').classList.remove('blink')
-        }, 2000);
+        blink(document.querySelector('#search input[type=text]:required'), "Requester: search Query Can Not be Null.");
     }
+}
+function initSearch() {
+    /*
+    <div class="search-container" id="search">
+        <input type="text" placeholder="Search SongName.." name="search" required>
+        <button><i class="fa fa-search"></i></button>
+    </div>
+    */
+    let searchContiner = document.getElementById('search');
+    if (searchContiner) {
+        searchContiner.remove();
+    }
+    searchContiner = document.createElement('div');
+    searchContiner.id = "search";
+    searchContiner.className = 'search-container';
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = "Search SongName..";
+    input.name = "search";
+    input.required = true;
+    let btn = document.createElement("button");
+    btn.addEventListener("click", menuFOS);
+    let icon = document.createElement('i');
+    icon.className = 'fa fa-search';
+    btn.append(icon);
+    searchContiner.append(input, btn);
+    document.getElementById('title').insertAdjacentElement('afterend', searchContiner);
 }
 window.onload = () => {
     console.log(isKor);
+    initSearch();
     Localiztion();
-    document.querySelector('#search button').addEventListener("click", menuFOS);
     /*document.body.onload = ()=> {
         //document.getElementById('title').innerText = 'akaka'
         console.log(isKor);
